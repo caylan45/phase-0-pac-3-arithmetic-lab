4e20b9e3e5555b1ed099340fbc6859f04b9dacd7
caylan wilcox <caylan45@yahoo.com>
2022-11-19 16:23:31 -0600

    commit tree e60ada3696520eba2efdacd7aa770021d1d29d34

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(a, b)
+{
+    return a+b;
+}
+    add(a,b);
+
+function subtract(a, b)
+{
+     a-b;
+}
+    console.log(subtract(a,b));
+
+function multiply(a, b)
+{
+    return a*b;
+}
+    console.log(multiply(a, b));
+
+function divide(a, b)
+{
+    return a/b;
+}
+    console.log(divide(a,b));
+
+function increment(n)
+{
+    return ++n;
+}
+    console.log(increment(n));
+
+function decrement(n)
+{
+    return --n;
+}
+    console.log(decrement(n));
+
+function makeInt(n)
+{
+    return parseInt(n, 10) ;
+}
+    console.log(makeInt(n));
+
+function preserveDecimal(n)
+{
+    return parseFloat(n) ;
+}
+    console.log(preserveDecimal(n));
